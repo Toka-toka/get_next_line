860e755dcecf8d2d8a47c7279b712256b2bba3f7
sedric <sedric@student.21-school.ru>
2020-07-29 18:36:44 +0300

    best ot the best

--- get_next_line.c
+++ get_next_line.c
@@ -1,29 +1,25 @@
 # include "get_next_line.h"
-# include <stdio.h>
 
-int	join_call(char **line, char **buff, int *start)
+int	join_call(char **line, char *buff, int *start)
 {
-	int sym_plase;
+	int sym_index;
 
-	sym_plase = strchr_int(*buff + *start, '\n');
-	if (sym_plase != -1)
-		buff[0][sym_plase + *start] = '\0';
-	*line = strjoin_free(*line, *buff + *start);
+	sym_index = strchr_int(buff, '\n');
+	if (sym_index != -1)
+		buff[sym_index] = '\0';
+	*line = strjoin_free(*line, buff);
 	if (*line == NULL)
 		return (-1);
-	if (sym_plase == -1)
-	{
-		*start = 0;
+	if (sym_index == -1)
 		return(2);
-	}
-	*start += (sym_plase + 1);
+	*start += (sym_index + 1);
 	return(1);
 }
 
 int	read_call(int fd, char **buff)
 {
 	int status;
-
+	
 	if (!(*buff = malloc((BUFFER_SIZE +1) * sizeof(char))))
 		return(-1);
 	status = read(fd, *buff, BUFFER_SIZE);
@@ -47,9 +43,12 @@ int	get_next_line(int fd, char **line)
 		if(start == 0)
 			status = read_call(fd, &buff);
 		if (status > 0)
-			status = join_call(line, &buff, &start);
-		if (status != 1)
+			status = join_call(line, buff + start, &start);
+		if (status != 1 || start == BUFFER_SIZE)
+		{
+			start = 0;
 			free(buff);
+		}
 	}
 	return(status);
 }
